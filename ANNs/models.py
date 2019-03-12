7b6a01d597a0b67706505d68cef3acb0d29b28a4
Alexander Stasik <35530131+AlexStasik@users.noreply.github.com>
2019-03-12 13:43:37 +0100

    more

--- ANNs/models.py
+++ ANNs/models.py
@@ -24,32 +24,7 @@ def save_model(model, save_dir='models'):
     model.save(path)
 
 
-def generator(X, y, batch_size, lookback, return_full_stream=False, threashold=0.5):
-    batch_features = np.zeros((batch_size, lookback, 1))
-    batch_labels = np.zeros((batch_size, 2))
-    if return_full_stream:
-        batch_labels_full = np.zeros((batch_size, lookback, 2))
-    while True:
-        for i in range(batch_size):
-            index = np.random.randint(0, X.shape[0]-lookback)
-            a = X[index:index+lookback]
-            batch_features[i] = np.reshape(a, (a.shape[0], -1))
-            val = y[index:index+lookback]
-            if return_full_stream:
-                batch_labels_full[i] = y[index:index+lookback]
-            rate = np.sum(val, axis=0)
-            ratio = rate[1] / float(val.shape[0])
-            if ratio>=threashold:
-                batch_labels[i] = np.array([0, 1])
-            else:
-                batch_labels[i] = np.array([1, 0])
-        if return_full_stream:
-            yield batch_features, batch_labels, batch_labels_full
-        else:
-            yield batch_features, batch_labels
-
-
-def generate_model(input_shape, padding='same'):
+def generate_model_CNN(input_shape, padding='same'):
     keras.backend.clear_session()
 
     input_layer = keras.layers.Input(shape=input_shape)
@@ -98,31 +73,22 @@ def plot_performance(history):
     plt.show()
 
 
-def decode(value, threshold=0.992):
-    mask = value[:,1]>threshold
-    y_pred_int = np.array(mask, dtype=int)
-    return y_pred_int
-
-def decode2(value, threshold=0.5):
-    return np.argmax(value, axis=-1)
-
-
 def make_accuracy_matrix_plot(model, validate_generator, ref='truth'):
     X_trial, y_trial, = next(validate_generator)
     res = model.predict(X_trial)
 
     print(y_trial)
     print(res)
-    
+
     y = decode(y_trial)
     res = decode(res)
 
-   
+
     plt.figure()
     plt.plot(y)
     plt.plot(res)
     plt.show()
-    
+
     res_matrix = np.zeros((2,2))
     if ref=='truth':
         for i, true_label in enumerate(set(y)):
